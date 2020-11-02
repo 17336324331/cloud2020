da44d90474735324b604d078b4e9e46674e02776
17336324331@163.com <17336324331@163.com>
2020-11-02 23:31:46 +0800

    消费者注册到注册中心

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderApplication.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderApplication.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cry
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
