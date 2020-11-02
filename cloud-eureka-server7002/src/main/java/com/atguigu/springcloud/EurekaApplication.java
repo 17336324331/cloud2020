24bda2c04ec0347f233234c2d32302510008eff6
17336324331@163.com <17336324331@163.com>
2020-11-02 23:32:30 +0800

    Eureka集群,即两个Eureka互相注册

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author cry
+ * @date 2020年11月02日 23:19
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
